92675ba4bf9f21bc465f8202347db4dd70282a20
unknown <344054424@TDSB.ON.CA>
2023-05-15 14:37:43 -0400

    added js, html, classes

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,85 +1,186 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "vue";
+import axios from "axios";
+
+const key = 
+
+document.getElementById("getbutton").addEventListener("click", getmovie);
+
+function getmovie() {
+  let movieId = parseInt(document.getElementById("list").value);
+
+  let search = axios.get("https://api.themoviedb.org/3/movie/" + movieId, {
+    params: {
+      api_key: key,
+      append_to_response: "videos",
+    },
+  });
+
+  search.then((movieData) => {
+    document.getElementById("poster").src =
+      "https://image.tmdb.org/t/p/w500" + movieData.data.poster_path;
+
+    document.getElementById("title").innerHTML = movieData.data.original_title;
+    document.getElementById("tagline").innerHTML = movieData.data.tagline;
+    document.getElementById(
+      "status"
+    ).innerHTML = `${movieData.data.status}: ${movieData.data.release_date}`;
+    document.getElementById(
+      "language"
+    ).innerHTML = `Languages: ${movieData.data.language}`;
+    document.getElementById(
+      "runtime"
+    ).innerHTML = `Run-time: ${movieData.data.runtime} minutes`;
+    document.getElementById(
+      "popularity"
+    ).innerHTML = `Popularity: ${movieData.data.popularity}`;
+    document.getElementById(
+      "voteAverage"
+    ).innerHTML = `Vote Average: ${movieData.data.vote_average}`;
+    document.getElementById(
+      "voteCount"
+    ).innerHTML = `Vote Count: ${movieData.data.vote_count}`;
+    document.getElementById(
+      "budget"
+    ).innerHTML = `Budget: $${movieData.data.budget}`;
+    document.getElementById("overview").innerHTML = movieData.data.overview;
+    document.getElementById("trailer").src =
+      "https://www.youtube.com/embed/" +
+      movieData.data.videos.results
+        .filter((trailer) => trailer.type === "Trailer")
+        .at(0).key;
+  });
+}
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <head>
+    <div id="pagetop">
+      <select id="list" name="Select">
+        <option value="809">Shrek 2</option>
+        <option value="76600">Avatar: The Way of Water</option>
+        <option value="505642">Black Panther: Wakanda Forever</option>
+        <option value="502356">The Super Mario Bros. Movie</option>
+        <option value="594767">Shazam! Fury of the Gods</option>
+        <option value="603692">John Wick: Chapter 4</option>
+        <option value="640146">Ant-Man and the Wasp: Quantumania</option>
+        <option value="378064">A Silent Voice: The Movie</option>
+        <option value="2109">Rush Hour</option>
+        <option value="9502">Kung Fu Panda</option>
+      </select>
+      <button id="getbutton">Get</button>
     </div>
-  </header>
+  </head>
 
-  <RouterView />
+
+  <body>
+    <div class="container">
+      <div id="containertop">
+        <div id="left">
+          <img id="poster" src="" />
+        </div>
+        <div id="right">
+          <h1 id="title"></h1>
+          <h3 id="tagline"></h3>
+          <p id="overview"></p>
+        </div>
+      </div>
+      <div id="containerbot">
+        <h3 id="status"></h3>
+        <p id="language"></p>
+        <p id="runtime"></p>
+        <p id="popularity"></p>
+        <p id="voteAverage"></p>
+        <p id="voteCount"></p>
+        <p id="budget"></p>
+      </div>
+      <iframe id="trailer"></iframe>
+    </div>
+  </body>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+* {
+  font-family: "Poppins", sans-serif;
+  padding: 0;
+  margin: 5px;
+  box-sizing: border-box;
+  background-color: rgb(24, 23, 23);
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
+.container {
+  margin-top: 20px;
   width: 100%;
-  font-size: 12px;
+  align-items: center;
+}
+
+#pagetop {
+  display: flex;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 20px;
   text-align: center;
-  margin-top: 2rem;
+  width: 20%;
+  height: 40px;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+#list {
+  color: azure;
+  border-radius: 5px;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+#getbutton {
+  margin-left: 5px;
+  color: azure;
+  border-radius: 5px;
 }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+#containertop {
+  height: 100vh;
+  width: 100%;
+  justify-content: space-between;
+  display: flex;
 }
 
-nav a:first-of-type {
-  border: 0;
+.left {
+  width: 40%;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+img {
+  border-radius: 8px;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.right {
+  width: 55%;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+h1 {
+  color: rgb(225, 218, 218);
+  font-size: 60px;
+}
 
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
+h3 {
+  color: rgb(175, 169, 169);
+  font-size: 35px;
+}
 
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+p {
+  color: rgb(158, 156, 156);
+  font-size: 20px;
+}
+
+#containerbot {
+  overflow: hidden;
+  width: 100%;
+}
+
+iframe {
+  bottom: 0px;
+  float: right;
+  border-radius: 8px;
+  position: sticky;
+  aspect-ratio: 16/9;
+  width: 50%;
 }
 </style>
